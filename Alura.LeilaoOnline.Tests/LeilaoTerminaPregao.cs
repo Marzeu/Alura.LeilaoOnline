1d1580cb27ddca4aecc245f4cbde1dcfda0f120b
Marzeu <marzahell@hotmail.com>
2021-04-09 09:58:09 -0300

    refatorando nomes e adicionando novos testes

--- Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -6,13 +6,13 @@ using Xunit;
 
 namespace Alura.LeilaoOnline.Tests
 {
-    public class LeilaoTestes
-    {   
+    public class LeilaoTerminaPregao
+    {
         [Theory]
-        [InlineData(1000, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
         [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
         [InlineData(800, new double[] { 800 })]
-        public void LeilaoComVariosLances(
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(
             double valorEsperado,
             double[] ofertas)
         {
@@ -33,7 +33,7 @@ namespace Alura.LeilaoOnline.Tests
             Assert.Equal(valorEsperado, valorObtido);
         }
         [Fact]
-        public void LeilaoSemLance()
+        public void RetornaZeroDadoLeilaoSemLances()
         {
             //Arranjo - Cenário           
             var leilao = new Leilao("Van Gogh");
@@ -46,6 +46,6 @@ namespace Alura.LeilaoOnline.Tests
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
-        }       
+        }
     }
 }
